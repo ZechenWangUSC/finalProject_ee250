b8786568723360d1a2c93fea601aa9d74307bc3f
Zechen Wang <zechenwa@usc.edu>
2020-11-08 21:06:26 -0800

    [feat]added temp sensor

--- sensors.py
+++ sensors.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import math
 
 sys.path.append('./Software/Python/')
 sys.path.append('./Software/Python/grove_rgb_lcd')
@@ -8,8 +9,8 @@ import grovepi
 import grove_rgb_lcd as lcd
 
 if __name__ == '__main__':
-    PORT = 4    # D4
-    meter = 0   # A0
+    UR = 4    # Ultrasonic ranger, D4
+    DHT = 3     # humidity and temp sensor, D3
     lcd.setRGB(153,255,51)
 
     grovepi.pinMode(meter, "INPUT")
@@ -19,13 +20,16 @@ if __name__ == '__main__':
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
 
-        deg = round( grovepi.analogRead(meter) * 300 / 1023) #range: [0,300]
-        print("Rot Sensor:" + str(deg))
-
-        dist = grovepi.ultrasonicRead(PORT)
+        dist = grovepi.ultrasonicRead(UR)
         print("Distance:" + str(dist))
-        
-        if dist < deg:
-            lcd.setText_norefresh("%3dcm OBJ PRES\n%3dcm" % (deg,dist))
-        else:
-            lcd.setText_norefresh("%3dcm          \n%3dcm" % (deg,dist))
+
+        try:
+
+            [temp,humidity] = grovepi.dht(DHT,0)  
+            if math.isnan(temp) == False and math.isnan(humidity) == False:
+                print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
+         
+            except IOError:
+                print ("Error")
+
+        lcd.setText_norefresh("dist = %3dcm \ntemp=%.02f hum=%.02f" % (deg,temp.humidity))
