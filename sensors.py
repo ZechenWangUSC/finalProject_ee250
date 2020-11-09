0b0b23f67c2f6c08ba0dadc4050fb6086c3e12cc
Zechen Wang <zechenwa@usc.edu>
2020-11-08 23:26:37 -0800

    [fix]I2C Race

--- sensors.py
+++ sensors.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import math
+import threading
 
 sys.path.append('./Software/Python/')
 sys.path.append('./Software/Python/grove_rgb_lcd')
@@ -15,6 +16,7 @@ if __name__ == '__main__':
     lcd.setRGB(153,255,51)
 
     grovepi.pinMode(LIGHT,"INPUT")
+    lock = threading.Lock()
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
@@ -22,14 +24,17 @@ if __name__ == '__main__':
         time.sleep(0.2)
 
         try:
-            dist = grovepi.ultrasonicRead(UR)
+            with lock:
+                dist = grovepi.ultrasonicRead(UR)
             print("Distance:" + str(dist))
 
-            [temp,humidity] = grovepi.dht(DHT,0)  
+            with lock:
+                [temp,humidity] = grovepi.dht(DHT,0)  
             if math.isnan(temp) == False and math.isnan(humidity) == False:
                 print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
          
-            light_value = grovepi.analogRead(LIGHT)
+            with lock:
+                light_value = grovepi.analogRead(LIGHT)
             print("Light:" + str(light_value))
 
         except IOError:
