b360863821a8aae0e8c4551e073b6d0744deea1e
Zechen Wang <zechenwa@usc.edu>
2020-11-09 01:31:26 -0800

    [fix]syntax

--- sensors.py
+++ sensors.py
@@ -14,29 +14,28 @@ if __name__ == '__main__':
     DHT = 3     # humidity and temp sensor, D3
     LIGHT = 0   # light sensor, A0
     lcd.setRGB(153,255,51)
+    lcd.clear()
 
     grovepi.pinMode(LIGHT,"INPUT")
     lock = threading.Lock()
 
     while True:
-        #So we do not poll the sensors too quickly which may introduce noise,
-        #sleep for a reasonable time of 200ms between each iteration.
-        time.sleep(0.2)
+        time.sleep(5)   #polls every 5 seconds
 
         try:
-            with lock:
+            with lock:  #read ultrasonic ranger
                 dist = grovepi.ultrasonicRead(UR)
                 time.sleep(0.2)
             print("Distance:" + str(dist))
 
-            with lock:
+            with lock:  #read humidity&temp sensor
                 [temp,humidity] = grovepi.dht(DHT,0)
                 time.sleep(0.2)
   
             if math.isnan(temp) == False and math.isnan(humidity) == False:
                 print("temp = %.02f C humidity =%.02f%%"%(temp, humidity))
          
-            with lock:
+            with lock:  #read light sensor
                 light_value = grovepi.analogRead(LIGHT)
                 time.sleep(0.2)
             print("Light:" + str(light_value))
@@ -44,4 +43,5 @@ if __name__ == '__main__':
         except IOError:
             print ("Error")
 
+        #lcd display
         lcd.setText_norefresh("dist=%3dcm \nT=%.01f H=%.01f" % (dist,temp,humidity))
